dba0a82924fbdbfa6d84e1387b27f555fc9036f1
miriku <me@miriku.com>
2017-05-17 13:50:59 -0700

    game info table built

--- buildGameInfoTable.php
+++ buildGameInfoTable.php
@@ -1,9 +1,34 @@
 <?php
+  $dbh_bgg;
+  require("lib_mysqlConnect.php");
+
   // for each game load xml
-    
-  // parse out mechanics
-    
-  // parse out categories
-    
-  // save as string (maybe redo as bools if slow)
-?>
+  $q = $dbh_bgg->prepare("SELECT xml, id FROM games");
+  $q->execute();
+
+  $count = 0;
+
+  while($row=$q->fetch())
+  {
+    $id = $row['id'];
+    $xml = $row['xml'];
+    $xml_lines = preg_split("/\\n/", $xml);
+    $running_string = "";
+    foreach($xml_lines as $xml_line)
+    {
+      if(!preg_match("/boardgamecategory/", $xml_line)
+          &&
+         !preg_match("/boardgamemechanic/", $xml_line))
+        continue;
+
+      $value = preg_replace("/.*value=\\\"(.*?)\\\".*/", '$1', $xml_line);
+
+      $running_string .= $value . "||";
+    }
+    // save as string (maybe redo as bools if slow)
+    $q2 = $dbh_bgg->prepare("UPDATE games SET string='$running_string' WHERE id=$id");
+    $q2->execute();
+
+    $count++;
+    print "\r$count/90402";
+  }
