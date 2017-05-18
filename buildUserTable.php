c7524277a3a5dd1f8f3807a97d2bc15c611fd7b3
miriku <me@miriku.com>
2017-05-17 21:27:06 -0700

    test of build for user table

--- buildUserTable.php
+++ buildUserTable.php
@@ -2,21 +2,63 @@
   $dbh_bgg;
   require("lib_mysqlConnect.php");
 
-
   // nuke tables
+  $q = $dbh_bgg->prepare("DELETE FROM users");
+  $q->execute();
+
+  $count = 0;
 
 	// for each rating
+  for($i=0; $i<11133474; $i++)
+  {
+    $q = $dbh_bgg->prepare("SELECT username, game, rating FROM ratings LIMIT $i, 1");
+    $q->execute();
 
-	// create user if not exist
+    while($row=$q->fetch())
+    {
+      // create user if not exist
+      $username = $row['username'];
+      $game = $row['game'];
+      $rating = $row['rating'];
 
-	// check rating of game
+      $q2 = $dbh_bgg->prepare("SELECT COUNT(*) AS exist FROM users WHERE Name='$username'");
+      $q2->execute();
+      $row2=$q2->fetch();
 
-	// load game stats
+      if($row2['exist']==0)
+      {
+        $q3 = $dbh_bgg->prepare("INSERT INTO users(`Name`) VALUES('$username')");
+        $q3->execute();
+      }
 
-	// iterate each game tag
+      // load game stats
+      $q4 = $dbh_bgg->prepare("SELECT string FROM games WHERE id='$game'");
+      $q4->execute();
 
-	// store the info per tag
+      $row4 = $q4->fetch();
+      $string = $row4['string'];
 
-	// figure out new vector for user
+      // iterate each game tag
+      $tags = explode("||", $string);
 
-	// update count for user vector
+      $q5 = $dbh_bgg->prepare("SELECT * FROM users WHERE Name=\"$username\"");
+      $q5->execute();
+      $row5=$q5->fetch();
+
+      foreach($tags as $tag)
+      {
+        if(""==$tag) continue;
+
+        $oldVal = $row5["$tag"];
+        $oldCount = $row5["$tag Count"];
+        $newCount = $oldCount+1;
+        $newVal = (($oldVal*$oldCount + $rating)/$newCount);
+
+        // figure out magnitude of component of new vector for user
+        $q6 = $dbh_bgg->prepare("UPDATE users SET `$tag`='$newVal', `$tag Count`='$newCount' WHERE Name=\"$username\"");
+        $q6->execute();
+      }
+    }
+    $count++;
+    print "\r$count/11133474";
+  }
